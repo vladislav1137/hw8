883b03b9a87d7b456c189b66ab916eab028c940a
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-21 11:16:59 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,19 @@ public class Main {
         q[1]=2;
         q[2]=3;
         double[] w = {1.57,7.654,9.986};
-        int[] e = {1,2,3,4,5,6,7,8,9,1,2,3};
+        int[] e = {1,2,3,4,5};
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        int[] q = new int[3];
+        q[0]=1;
+        q[1]=2;
+        q[2]=3;
+        double[] w = {1.57,7.654,9.986};
+        int[] e = {1,2,3,4,5};
+        System.out.println(q[0] + ", " + q[1] + ", " + q[2]);
+        System.out.println(w[0] + ", " + w[1] + ", " + w[2]);
+        System.out.println(e[0] + ", " + e[1] + ", " + e[2] + ", " + e[3] + ", " + e[4]);
     }
 }
