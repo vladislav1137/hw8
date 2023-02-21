49c71c146cdbce47cbaad1581a047a13bacca29c
vladislav1137 <mr.vladislav220@yandex.ru>
2023-02-21 09:47:51 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int[] q = new int[3];
+        q[0]=1;
+        q[1]=2;
+        q[2]=3;
+        double[] w = {1.57,7.654,9.986};
+        int[] e = {1,2,3,4,5,6,7,8,9,1,2,3};
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+    }
+}
